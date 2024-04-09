72e645a304a700bc56e744c5e3443bc6c15c67ae
multiverseweb <multiverse.letter@gmail.com>
2024-04-10 03:09:39 +0530

    updated style

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 console.log("Tejas' Codes :)");
+function copy() {
+    navigator.clipboard.writeText("https://multiverseweb.github.io/tejas-links/");
+    document.getElementById("copy").innerHTML = "Link Copied!&nbsp;✓";
+}
