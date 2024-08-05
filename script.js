2a2760691378b25b8f855db724cf5c0af38691d2
multiverseweb <multiverse.letter@gmail.com>
2024-08-06 01:26:06 +0530

    randomized motion

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
 console.log("Tejas' Codes :)");
+
 function copy() {
     navigator.clipboard.writeText("tejas-links.netlify.app");
-    document.getElementById("copy").innerHTML = "Link Copied!&nbsp;✓";
+    document.getElementById("copy").innerHTML = "Link Copied!✓";
 }
+
+function moveElementRandomly(element) {
+    const screenWidth = window.innerWidth - element.clientWidth;
+    const screenHeight = window.innerHeight - element.clientHeight;
+
+    const randomX = Math.random() * screenWidth;
+    const randomY = Math.random() * screenHeight;
+
+    element.style.left = `${randomX}px`;
+    element.style.top = `${randomY}px`;
+}
+
+function startRandomMovement() {
+    const element1 = document.querySelector('.element1');
+    const element2 = document.getElementById('e2');
+    moveElementRandomly(element1);
+    moveElementRandomly(element2);
+
+    setInterval(() => {
+        moveElementRandomly(element1);
+        moveElementRandomly(element2);
+    }, 3000); // move every 3 seconds
+}
+
+window.onload = startRandomMovement;
